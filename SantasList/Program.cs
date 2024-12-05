47e99ece7303cedb527cd1beab4d1a98aae9f70b
Jeffrey Seneff <jeffsnff@gmail.com>
2024-12-05 08:08:33 -0500

    First commit for Santas List C# project

---
+++ SantasList/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SantasList{
+  internal class Program{
+    static void Main(string[] args){
+      Console.WriteLine("Hello from a new project for December 2024");
+    }
+  }
+}
