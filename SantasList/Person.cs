e089d4b2170d1dd6dc55c14e22ffabd9367b30cd
Jeffrey Seneff <jeffsnff@gmail.com>
2024-12-05 08:35:55 -0500

    Created scaffolding of Person object

--- SantasList/Person.cs
+++ SantasList/Person.cs
@@ -2,6 +2,22 @@ using System;
 
 namespace SantasList{
   public class Person{
+    private string name;
+    /*
+      Can I make address an Object as well?
+      Would have street, street2, city, state, zipcide
+    */
+    // private string address;
+    private string behavior;
+    private string present;
+    private Boolean status;
+
+    public Person(string name, string behavior, Boolean status){
+      this.name = name;
+      this.behavior = behavior;
+      this.status = status;
+    }
+
     
   }
 }
