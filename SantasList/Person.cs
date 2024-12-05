10bcd8a0a538742c575c9c16e6121619a70d8f60
Jeffrey Seneff <jeffsnff@gmail.com>
2024-12-05 08:54:37 -0500

    Added ability to check and update delivered status and ability to add a present

--- SantasList/Person.cs
+++ SantasList/Person.cs
@@ -12,17 +12,27 @@ namespace SantasList{
     // Can I make behavior an enum? 
     private string behavior;
     private string present;
-    private Boolean status;
+    private Boolean deliveredStatus;
 
-    public Person(string name, string behavior, Boolean status){
+    public Person(string name, string behavior, Boolean deliveredStatus){
       this.name = name;
       this.behavior = behavior;
-      this.status = status;
+      this.deliveredStatus = deliveredStatus;
     }
-
     public override string ToString()
     {
-      return "Name: "+this.name+"\r\n"+"Naughty or Nice? "+this.behavior+"\r\n"+"Delivered Present? "+this.status;
+      return "Name: "+this.name+"\r\n"+"Naughty or Nice? "+this.behavior+"\r\n"+"Delivered Present? "+this.deliveredStatus;
+    }
+
+    public Boolean DeliveredStatus(){
+      return this.deliveredStatus;
+    }
+    public void UpdateDeliveredStatus(Boolean deliveredStatus){
+      this.deliveredStatus = deliveredStatus;
+    }
+    public string AddPresent(string present){
+      this.present = present;
+      return this.name+" present will be "+this.present;
     }
   }
 }
