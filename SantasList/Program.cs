ede1aa939d8abb08419cec8a051f938286af57f2
Jeffrey Seneff <jeffsnff@gmail.com>
2024-12-06 08:24:06 -0500

    Added Greetings message and command method

--- SantasList/Program.cs
+++ SantasList/Program.cs
@@ -3,7 +3,33 @@
 namespace SantasList{
   internal class Program{
     static void Main(string[] args){
-      Console.WriteLine("Hello from a new project for December 2024");
+      List<Person> SantaList = new List<Person>();
+      string command;
+      string name;
+      string behavior;
+      string deliveredStatus;
+
+      command = Greetings();
+
+      
+
+
+      
     }
+
+    static string Greetings(){
+      Console.WriteLine("Welcome to Santas List!");
+      Console.WriteLine("Enter one of the following:");
+      Console.WriteLine("- 'Add' to add names to list");
+      Console.WriteLine("- 'Update' to update a name in list");
+      Console.WriteLine("- 'View All' to view all names in list");
+      Console.Write("Command Entry : ");
+
+      return Console.ReadLine();
+
+    }
+
+
+
   }
 }
